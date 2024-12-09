077bb2b4571eec4c770614853df0615679e84def
amald <amald@AMAL>
2024-12-09 14:56:07 -0500

    Add project files.

---
+++ VideoGameApi/Controllers/VideoGameController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VideoGameApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoGameController : ControllerBase
+    {
+        static private List<VideoGame> videoGames = new List<VideoGame>
+        {
+            new VideoGame
+            { Id = 1,
+                Title = "Spider-Man 2",
+                Platform = "PS5",
+                Developer = "Insomniac Games",
+                Publisher = "Sony Interactive Entertainment"
+            },
+            new VideoGame
+            { Id = 2,
+                Title = "The Legend Of Zelda: Breath of the Wild",
+                Platform = "Nintendo Switch",
+                Developer = "Nintendo EPD",
+                Publisher = "Nintendo"
+            },
+            new VideoGame
+            { Id = 3,
+                Title = "Cyberpunk 2077",
+                Platform = "PC",
+                Developer = "CD Projekt Red",
+                Publisher = "CD Projekt"
+            }
+        };
+
+        [HttpGet]
+        public ActionResult<List<VideoGame>> GetVideoGames()
+        {
+            return Ok(videoGames);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<VideoGame> GetVideoGameById(int id)
+        {
+            var game = videoGames.FirstOrDefault(g => g.Id == id);
+            if (game == null) { return NotFound(); }
+            return Ok(game);
+        }
+
+        [HttpPost]
+        public ActionResult <VideoGame> AddVideoGame(VideoGame videoGame)
+        {
+            if (videoGame == null) { return BadRequest(); };
+            videoGame.Id = videoGames.Max(g => g.Id)+1;
+            videoGames.Add(videoGame);
+            return CreatedAtAction(nameof(AddVideoGame),new {id = videoGame.Id }, videoGame);
+        }
+    }
+}
